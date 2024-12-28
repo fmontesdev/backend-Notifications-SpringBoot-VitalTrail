b21c1952bcb9c36c535d09a2905da544908e45ec
fmontesdev <f.montesdoria@gmail.com>
2024-12-28 14:51:46 +0100

    Implementado manejador de excepciones

---
+++ src/main/java/com/springboot/resend/domain/exception/NotificationException.java
@@ -0,0 +1,18 @@
+package com.springboot.resend.domain.exception;
+
+import lombok.Getter;
+
+@Getter
+public class NotificationException extends RuntimeException {
+    private final Error error;
+    
+    public NotificationException(Error error) {
+        super(error.getMessage());
+        this.error = error;
+    }
+
+    public NotificationException(Error error, Throwable cause) {
+        super(error.getMessage(), cause);
+        this.error = error;
+    }
+}
